1614aba7be8c7194547296ecf789033f59ced6b0
vruas <vitor-ruas@live.com>
2023-09-04 22:25:45 -0300

    configurando rotas, método createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProduto from './routes/EditarProdutos/index.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path:"/",
+        element: <Home/>
+      },
+      {
+        path:'/produtos',
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto/>
+      }
+    ]
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
